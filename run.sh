0d930319c8110c23961f119f0d9bf0e519d83deb
leros <zhleiyu@gmail.com>
2017-12-08 09:57:00 -0500

    Complie before running

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# Script to run particle filter!
+# Script to compile and run particle filter!
 #
 # Written by Tiffany Huang, 12/14/2016
+# Updated by Leo Lei, 12/08/2017
 #
 
-# Run particle filter
+# Compile and Run particle filter
 cd ./build
+cmake ..
+make
 ./particle_filter
